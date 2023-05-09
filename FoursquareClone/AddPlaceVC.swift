27b57fa3cbf6fa6f3edab9950eb3bad85ab4a01a
tahaturan <tahaaturann16@gmail.com>
2023-05-09 13:39:06 +0300

    Mekan ekeleme VC olusturuldu

---
+++ FoursquareClone/AddPlaceVC.swift
@@ -0,0 +1,32 @@
+//
+//  AddPlaceVC.swift
+//  FoursquareClone
+//
+//  Created by Taha Turan on 9.05.2023.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    
+    @IBOutlet weak var placeNameTextField: UITextField!
+    
+    @IBOutlet weak var placeTypeTextField: UITextField!
+    
+    @IBOutlet weak var placeAtmosphereTextField: UITextField!
+    
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func nextButton(_ sender: Any) {
+    }
+    
+
+}
