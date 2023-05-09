b01de89f5b5a144329c74caeba2865646b4c597e
tahaturan <tahaaturann16@gmail.com>
2023-05-09 14:11:36 +0300

    DetaylarVC Olusturuldu

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Taha Turan on 9.05.2023.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+    
+    @IBOutlet weak var detailsImageView: UIImageView!
+    
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    
+    @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    
+    @IBOutlet weak var detailsMapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+
+}
