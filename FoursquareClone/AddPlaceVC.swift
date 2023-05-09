eb4320f293ff23938c9fc4c9ab59edfd53ba18a1
tahaturan <tahaaturann16@gmail.com>
2023-05-09 14:19:58 +0300

    Gorsel Secme islemleri

--- FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/AddPlaceVC.swift
@@ -20,9 +20,15 @@ class AddPlaceVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
 
         
     }
+    
+
     
 
     @IBAction func nextButton(_ sender: Any) {
@@ -32,3 +38,18 @@ class AddPlaceVC: UIViewController {
 
 }
 
+//MARK: Resim secme islemleri
+extension AddPlaceVC:UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+    
+    @objc func chooseImage()  {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        placeImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+}
